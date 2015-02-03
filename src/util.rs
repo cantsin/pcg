34e90bc2703676facb1bb8d7bac31c4f94ad0294
James Tranovich <james@tranovi.ch>
2015-02-03 13:23:13 -0800

    Fisher-Yates shuffle algorithm.

--- src/util.rs
+++ src/util.rs
@@ -1,2 +1,13 @@
+use std::rand::{Rng};
 
 pub type Coords = (i32, i32);
+
+// Fisher-Yates algorithm
+pub fn shuffle<'a, R: Rng, T: Clone>(rng: &mut R, array: &'a mut [T]) {
+    let mut n = array.len();
+    while n > 1 {
+        let k = rng.gen_range(0, n);
+        n -= 1;
+        array.swap(n, k);
+    }
+}
