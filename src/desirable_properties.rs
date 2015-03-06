156dcd9bb883cfd811567be64e132c58e4b17e32
James Tranovich <james@tranovi.ch>
2015-03-06 15:17:06 -0800

    Start of growing maze paths.

--- src/desirable_properties.rs
+++ src/desirable_properties.rs
@@ -4,6 +4,7 @@ use genotype::{Genotype};
 use phenotype::{Seed};
 use config::{Config};
 
+use std::iter::{range_step};
 use rand::{Rng};
 
 #[derive(Clone, Debug)]
@@ -11,49 +12,76 @@ pub struct DesirableProperties {
     seed: Seed,
     room_number: u32,
     room_size: u32,
-    rooms: Vec<Room>,
     doors: u32,
     monsters: u32,
     path_length: u32,
     branching: u32,
     occupants: Vec<(Occupant, (u32, u32))>,
+    rooms: Vec<Room>,
+    mazes: Vec<Maze>,
 }
 
 #[derive(Clone, Debug)]
-pub struct Room {
+struct Room {
     x: u32,
     y: u32,
     w: u32,
     h: u32
 }
 
+#[derive(Clone, Debug)]
+struct Maze {
+    region: u32,
+    path: Vec<(u32, u32)>
+}
+
+impl Maze {
+    pub fn new(region: u32, x: u32, y: u32) -> Maze {
+        let path = vec!((x, y));
+        Maze {
+            region: region,
+            path: path
+        }
+    }
+}
+
 impl Room {
     pub fn random<T: Rng>(rng: &mut T, width: u32, height: u32, room_size: u32) -> Room {
-        let w: u32 = rng.gen_range(2, room_size);
-        let h: u32 = rng.gen_range(2, room_size);
+        let w: u32 = rng.gen_range(3, room_size);
+        let h: u32 = rng.gen_range(3, room_size);
         assert!(width >= w);
         assert!(height >= h);
         let x: u32 = rng.gen_range(1, width - w);
         let y: u32 = rng.gen_range(1, height - h);
         Room {
-            x: x,
-            y: y,
-            w: w,
-            h: h
+            x: Room::make_odd(x),
+            y: Room::make_odd(y),
+            w: Room::make_odd(w),
+            h: Room::make_odd(h),
         }
     }
 
-    pub fn intersects(rooms: &Vec<Room>, room: &Room) -> bool {
+    pub fn make_odd(n: u32) -> u32 {
+        if n % 2 == 0 { n - 1 } else { n }
+    }
+
+    pub fn intersects(&self, rooms: &Vec<Room>) -> bool {
         for r in rooms {
             let rw = r.x + r.w;
             let rh = r.y + r.h;
-            let roomw = room.x + room.w;
-            let roomh = room.y + room.h;
-            if roomw >= r.x && roomh >= r.y && room.x <= rw && room.y <= rh {
+            let roomw = self.x + self.w;
+            let roomh = self.y + self.h;
+            if roomw >= r.x && roomh >= r.y && self.x <= rw && self.y <= rh {
                 return true
             }
         }
         false
+    }
+
+    pub fn contains(&self, x: u32, y: u32) -> bool {
+        let roomw = self.x + self.w;
+        let roomh = self.y + self.h;
+        x >= self.x && x <= roomw && y >= self.y && y <= roomh
     }
 }
 
@@ -71,12 +99,13 @@ impl DesirableProperties {
             seed: seed.clone(),
             room_size: room_size,
             room_number: room_number,
-            rooms: vec![],
             doors: doors,
             monsters: monsters,
             path_length: path_length,
             branching: branching,
             occupants: vec![],
+            rooms: vec![],
+            mazes: vec![]
         }
     }
 
@@ -85,37 +114,48 @@ impl DesirableProperties {
 impl Genotype for DesirableProperties {
     fn initialize<T: Rng>(&self, rng: &mut T) -> DesirableProperties {
         let occupants = self.seed.random_occupants(rng).iter().take(self.monsters as usize).cloned().collect();
-        let mut rooms = self.rooms.clone();
+        // randomly generate rooms
+        let mut rooms = vec![];
         for _ in range(0, self.room_number) {
-            let room = Room::random(rng, self.seed.width, self.seed.height, self.room_size);
             for _ in range(0, 10) {
-                if !Room::intersects(&rooms, &room) {
+                let room = Room::random(rng, self.seed.width, self.seed.height, self.room_size);
+                if !room.intersects(&rooms) {
                     rooms.push(room);
                     break;
                 }
             }
         }
+        // fill in mazes
+        let mut mazes = vec![];
+        let mut region = 0;
+        for x in range_step(1, self.seed.width, 2) {
+            for y in range_step(1, self.seed.height, 2) {
+                let is_occupied = rooms.iter().fold(false, |accum, ref m| accum || m.contains(x, y));
+                if !is_occupied {
+                    let maze = Maze::new(region, x, y);
+                    mazes.push(maze);
+                    region += 1;
+                }
+            }
+        }
+        // find connectors
+        // remove dead ends
         DesirableProperties {
             seed: self.seed.clone(),
             room_number: self.room_number,
             room_size: self.room_size,
-            rooms: rooms,
             doors: self.doors,
             monsters: self.monsters,
             path_length: self.path_length,
             branching: self.branching,
             occupants: occupants,
+            rooms: rooms,
+            mazes: mazes,
         }
     }
 
-    fn mutate<T: Rng>(&mut self, rng: &mut T, percentage: f64) {
-        // randomly generate rooms
-
-        // fill in mazes
-
-        // find connectors
-
-        // remove dead ends
+    fn mutate<T: Rng>(&mut self, _: &mut T, _: f64) {
+        // mutate a certain % of the rooms and start again
     }
 
     fn generate(&self) -> Dungeon {
@@ -128,6 +168,12 @@ impl Genotype for DesirableProperties {
                 for j in range(room.y, room.y + room.h) {
                     dungeon.cells[i as usize][j as usize].tile = Some(floor.clone())
                 }
+            }
+        }
+        for maze in self.mazes.iter() {
+            for path in maze.path.iter() {
+                let (x, y): (u32, u32) = *path;
+                dungeon.cells[x as usize][y as usize].tile = Some(floor.clone());
             }
         }
         dungeon.clone()
