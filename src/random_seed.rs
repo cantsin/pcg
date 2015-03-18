0595ded7871ff779895b3344f892bcdbd5d7e10b
James Tranovich <james@tranovi.ch>
2015-03-18 13:42:13 -0700

    Fix random seed.

--- src/random_seed.rs
+++ src/random_seed.rs
@@ -23,12 +23,13 @@ impl Genotype for RandomSeed {
         let w = self.seed.width;
         let h = self.seed.height;
         let mut dungeon = Dungeon::new(w, h, None);
-        for i in 0..dungeon.width {
-            for j in 0..dungeon.height {
+        for i in 0..dungeon.width as u32 {
+            for j in 0..dungeon.height as u32 {
                 let tile = self.seed.tiles.choose(&mut rng).clone();
-                let occupant = self.seed.random_occupant(&mut rng, &tile).unwrap();
-                dungeon.set_occupant(i as u32, j as u32, &occupant);
-                dungeon.set_tile(i as u32, j as u32, &tile);
+                if let Some(occupant) = self.seed.random_occupant(&mut rng, &tile) {
+                    dungeon.set_occupant(i, j, &occupant);
+                }
+                dungeon.set_tile(i, j, &tile);
             }
         }
         dungeon.clone()
