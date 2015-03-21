e78af9c1e0958d351a9200a6fde2fe57c2b541f6
James Tranovich <james@tranovi.ch>
2015-03-21 09:16:18 -0700

    Temporary fix for sprite offsets.

--- src/sprite.rs
+++ src/sprite.rs
@@ -65,8 +65,8 @@ impl SpriteRect {
             rectangle: None,
             source_rectangle: Some([self.x * self.w,
                                     self.y * self.h,
-                                    self.w,
-                                    self.h])
+                                    self.x * self.w + self.w * 64,
+                                    self.y * self.h + self.h * 64])
         }
     }
 }
